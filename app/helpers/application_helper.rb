05a5314e62d4a89cfe39f45b4ff3c99bbad459b3
reo66 <8409.reo@gmail.com>
2020-08-30 18:18:58 +0900

    title OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else  
+      page_name + " | " + base_title
+    end
+  end  
 end
