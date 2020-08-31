9fad11dd0902a4024c3af2c64f6d9417369038bf
reo66 <8409.reo@gmail.com>
2020-08-31 22:46:49 +0900

    user.rb has_secure_password 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100},
                     format: { with: VALID_EMAIL_REGEX }
-                    uniqueness: true               
+                    uniqueness: true 
+  has_secure_password   
 end
