d4b27c08cd0a6c97de182815a0b12ef4dfac7694
reo66 <8409.reo@gmail.com>
2020-09-01 11:16:08 +0900

    AddColumnsToUsers migrationfile department,staff_id,card_id 追加

--- db/migrate/20200901020833_add_columns_to_users.rb
+++ db/migrate/20200901020833_add_columns_to_users.rb
@@ -1,4 +1,7 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :department, :string
+    add_column :users, :staff_id, :string
+    add_column :users, :card_id, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_134025) do
+ActiveRecord::Schema.define(version: 2020_09_01_020833) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2020_08_31_134025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "department"
+    t.string "staff_id"
+    t.string "card_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
