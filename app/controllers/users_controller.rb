6f3c8e0d48806e94caf0541e17551b734a313cbe
reo66 <8409.reo@gmail.com>
2020-09-01 21:58:45 +0900

    user_controller flash 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new 
