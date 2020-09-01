4a89b62d760351bf495818ac92516c78d5969b9c
reo66 <8409.reo@gmail.com>
2020-09-01 21:35:58 +0900

    uder_controller create 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new 
+  end 
 end
