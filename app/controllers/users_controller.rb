69db9da4ad7f46e5f1286230f18a65cacb05982c
reo66 <8409.reo@gmail.com>
2020-09-02 22:24:55 +0900

    user.controller def update 修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
       render :edit 
