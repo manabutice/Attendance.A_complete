5b057807b24b88d4e3cb391ce81e6ff92de2de10
reo66 <8409.reo@gmail.com>
2020-09-02 18:13:10 +0900

    session_helper def destroy 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = "ログアウトしました"
     redirect_to root_url
   end 
