09c5ea1644dcc804016700ffaa4dd82771434512
reo66 <8409.reo@gmail.com>
2020-09-02 22:19:09 +0900

    user_controller def update 修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-
+      redirect_to @user
     else
       render :edit 
     end 
