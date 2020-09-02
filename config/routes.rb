7ad1db9fb4fe751a6d352065c0d278f8f4855a6e
reo66 <8409.reo@gmail.com>
2020-09-02 10:06:44 +0900

    routes.rb 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#cerate'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
