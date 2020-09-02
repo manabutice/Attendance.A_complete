e749d5027b4f586ba9de8fc36d2985ed1e413eb4
reo66 <8409.reo@gmail.com>
2020-09-02 23:00:20 +0900

    session helper def redirect_back_or(default_url) & def store_location　追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,4 +39,14 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
