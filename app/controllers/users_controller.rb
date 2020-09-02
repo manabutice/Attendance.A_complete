3af8d4caec6a10210f48bcede49132f120ce389a
reo66 <8409.reo@gmail.com>
2020-09-02 22:16:28 +0900

    user_controller def update 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+
+    else
+      render :edit 
+    end 
+  end  
+
 
   
 
