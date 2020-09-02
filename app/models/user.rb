b6750248152f9f7771281ca91e0ee120e2e75378
reo66 <8409.reo@gmail.com>
2020-09-02 16:31:13 +0900

    user.rb def user.digest & def User.new_token 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
                     uniqueness: true    
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
