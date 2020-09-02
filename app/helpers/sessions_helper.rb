d05fbebd7c772ac8e58994aa8fbb59f5d66321c9
reo66 <8409.reo@gmail.com>
2020-09-02 17:53:38 +0900

    sessions.helper def remember追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end 
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def log_out
     session.delete(:user_id)
     @current_user = nil
