c3b359fca171d7b510b1c520f226d474345131f9
reo66 <8409.reo@gmail.com>
2020-09-02 22:46:21 +0900

    user_controller before_action 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -21,11 +21,9 @@ class UsersController < ApplicationController
   end 
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -43,6 +41,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation, :basic_work_time, :designation_work_start_time, :designation_work_end_time)
     end  
+
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 
     def logged_in_user
       unless logged_in?
