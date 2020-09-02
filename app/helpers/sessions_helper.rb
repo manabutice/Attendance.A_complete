f8461317dafbe7b0001cc0a961b2efc4501963e4
reo66 <8409.reo@gmail.com>
2020-09-02 15:52:34 +0900

    session_helper def loout 追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end 
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end 
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
@@ -11,5 +16,5 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
-  end 
+  end
 end
