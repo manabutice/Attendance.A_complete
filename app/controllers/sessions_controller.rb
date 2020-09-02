a167a3f35206ce293f47150f548a2bfbde76b903
reo66 <8409.reo@gmail.com>
2020-09-02 10:37:57 +0900

    session_controller cerate追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#cerate'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
   resources :users
 end
