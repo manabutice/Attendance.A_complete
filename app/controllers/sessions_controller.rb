fcf8e7af537bab2543b11592b4ffffda2a30fc48
reo66 <8409.reo@gmail.com>
2020-09-02 11:22:04 +0900

    session_controller def create 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else  
       flash.now[:danger] = "認証に失敗しました。"
       render :new
