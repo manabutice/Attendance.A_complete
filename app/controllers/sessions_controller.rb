185af883ade7c557a59fd35b78fc4a6298ec00cc
reo66 <8409.reo@gmail.com>
2020-09-02 11:02:07 +0900

    sessions_conerollr 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: paramas[:session][:email].dowacace)
-    if user && user.authenticate(paramas[:session][:password])
-      ridirect_to @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
     else  
+      flash[:danger] = "認証に失敗しました。"
       render :new
     end
   end
