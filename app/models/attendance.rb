e73f3da5e4b415909f702dd2d7af0f4408cb13cc
reo66 <8409.reo@gmail.com>
2020-09-03 11:30:56 +0900

    attendace.rb def finished_at_is_invalid_without_a_started_at 追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,4 +5,9 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
 
   validate :finished_at_is_invalid_without_a_started_at
+
+  def finished_at_is_invalid_without_a_started_at
+    errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+  
 end
