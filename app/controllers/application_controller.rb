75c607709f70df5414ae9d6a02412ae33237c810
reo66 <8409.reo@gmail.com>
2020-09-03 11:57:58 +0900

    application_controller 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,21 @@ class ApplicationController < ActionController::Base
   $days_of_the_week = %w{日 月 火 水 木 金 土}
 
   def set_one_month 
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
-    one_month = [*@first_day..@last_day] 
+    one_month = [*@first_day..@last_day]
+  
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
-
-    unless one_month.count == @attendances.count 
-      ActiveRecord::Base.transaction do 
+  
+    unless one_month.count == @attendances.count
+      ActiveRecord::Base.transaction do
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
-
-  rescue ActiveRecord::RecordInvalid 
+  
+  rescue ActiveRecord::RecordInvalid
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
