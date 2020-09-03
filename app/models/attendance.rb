de12e92b41b559cd592f32835b61467517abd40f
reo66 <8409.reo@gmail.com>
2020-09-03 11:30:08 +0900

    attendance.rb  validate :finished_at_is_invalid_without_a_started_at追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,6 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
+
+  validate :finished_at_is_invalid_without_a_started_at
 end
