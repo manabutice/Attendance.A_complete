0e53d191ae8fc1ea1030c2b6653cd23134121df2
reo66 <8409.reo@gmail.com>
2020-09-05 22:34:30 +0900

    user_controller def show追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
 
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
