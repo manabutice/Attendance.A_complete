7161a20f787cb27cb67b2f47b7707049841f0da6
reo66 <8409.reo@gmail.com>
2020-09-05 21:15:53 +0900

    attendance.controller 修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,7 +5,7 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
 
     if @attendance.started_at.nil?
-      if @attendance.update_attributes(started_at: Time.current.change(sec: 0)))
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます!"
       else
         flash[:info] = "勤怠登録に失敗しました。やり直してください。"
