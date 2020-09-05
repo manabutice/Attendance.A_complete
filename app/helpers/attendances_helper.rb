209af7027ed69c13c7c320ecbbf7b8c233f8e175
reo66 <8409.reo@gmail.com>
2020-09-05 22:09:29 +0900

    attendance_helper def attendance_state_start(attendance), def attendance_state_finish(attendance) 追加

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,9 +1,17 @@
 module AttendancesHelper
 
-  def attendance_state(attendance)
+  def attendance_state_start(attendance)
     # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
       return '出勤' if attendance.started_at.nil?
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    return false
+  end
+
+  def attendance_state_finish(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
       return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
     # どれにも当てはまらなかった場合はfalseを返します。
