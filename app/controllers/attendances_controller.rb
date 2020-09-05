1f4938d200a2c1647460d2cb96db787927dd8382
reo66 <8409.reo@gmail.com>
2020-09-05 22:12:34 +0900

    attendance_controller def update　追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,14 +3,20 @@ class AttendancesController < ApplicationController
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
-
+    # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
-        flash[:info] = "おはようございます!"
+        flash[:info] = "おはようございます！"
       else
-        flash[:info] = "勤怠登録に失敗しました。やり直してください。"
-      end 
-    end 
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    end
     redirect_to @user
   end
 end
