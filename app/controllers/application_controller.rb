dd4016f216e680443062390968a5b7e141c4f88c
reo66 <8409.reo@gmail.com>
2020-09-06 10:22:16 +0900

    def set_user def logged_in_user def correct_user user_controller→applicationcontoroller へ移行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < ActionController::Base
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインして下さい。"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end 
+
   def set_one_month 
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
