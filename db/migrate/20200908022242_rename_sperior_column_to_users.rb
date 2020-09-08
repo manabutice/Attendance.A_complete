bd64c4a9944d388717e0605bbaba565d7fed65a6
reo66 <8409.reo@gmail.com>
2020-09-08 11:26:09 +0900

    sperior カラム名修正

---
+++ db/migrate/20200908022242_rename_sperior_column_to_users.rb
@@ -0,0 +1,5 @@
+class RenameSperiorColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :sperior, :superior
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_021800) do
+ActiveRecord::Schema.define(version: 2020_09_08_022242) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 2020_09_08_021800) do
     t.string "department"
     t.string "staff_id"
     t.string "card_id"
-    t.datetime "basic_work_time", default: "2020-09-05 23:00:00"
-    t.datetime "designation_work_start_time", default: "2020-09-06 00:00:00"
-    t.datetime "designation_work_end_time", default: "2020-09-06 09:00:00"
+    t.datetime "basic_work_time", default: "2020-09-07 23:00:00"
+    t.datetime "designation_work_start_time", default: "2020-09-08 00:00:00"
+    t.datetime "designation_work_end_time", default: "2020-09-08 09:00:00"
     t.boolean "admin", default: false
     t.string "remember_digest"
-    t.boolean "sperior", default: false
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
