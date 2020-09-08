b45d060a3a523d082eb3359545e4794341edd834
reo66 <8409.reo@gmail.com>
2020-09-08 11:46:05 +0900

    管理者,一般,上長追加

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+  User.create!(name: "管理者",
+              email: "sample@email.com",
+              password: "password",
+              password_confirmation: "password",
+              department: "管理者",
+              staff_id: 0,
+              card_id: 0,
+              admin: true)
+
+              User.create!(name: "上長A",
+                email: "sample1@email.com",
+                password: "password",
+                password_confirmation: "password",
+                department: "役員",
+                staff_id: 1,
+                card_id: 1,
+                superior: true)
+
+                User.create!(name: "上長B",
+                  email: "sample2@email.com",
+                  password: "password",
+                  password_confirmation: "password",
+                  department: " 役員",
+                  staff_id: 2,
+                  card_id: 2,
+                  superior: true)
+
+                  User.create!(name: "一般A",
+                    email: "sample3@email.com",
+                    password: "password",
+                    password_confirmation: "password",
+                    department: "正職員",
+                    staff_id: 3,
+                    card_id: 3)
+
+                    User.create!(name: "一般B",
+                      email: "sample4@email.com",
+                      password: "password",
+                      password_confirmation: "password",
+                      department: "正職員",
+                      staff_id: 4,
+                      card_id: 4)
