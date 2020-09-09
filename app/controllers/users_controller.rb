2b3ece0ab679a0972ee64f7697e53b39ff128be5
reo66 <8409.reo@gmail.com>
2020-09-09 10:29:59 +0900

    seed.OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :edit_basic_info]
-  before_action :logged_in_user, only: [:index,:show, :edit, :update, :edit_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :destroy]
+  before_action :logged_in_user, only: [:index,:show, :edit, :update, :destroy, :edit_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :set_one_month, only: :show
   before_action :admin_user, only: [:index,:destroy, :edit_basic_info]
