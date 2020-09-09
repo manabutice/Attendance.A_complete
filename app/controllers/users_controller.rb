27b5be3efa7f43936bf75f3949da21bf99310558
reo66 <8409.reo@gmail.com>
2020-09-09 21:25:40 +0900

    _form.html.erb コード追加修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :destroy]
+  before_action :set_user, only: [:show, :edit, :index, :update, :edit_basic_info, :destroy]
   before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :edit_basic_info]
   before_action :correct_user, only: [:update]
   before_action :set_one_month, only: :show
