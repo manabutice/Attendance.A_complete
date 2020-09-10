2ed7b6b71ac5a0fbf38d4b16cf15102f3532844d
reo66 <8409.reo@gmail.com>
2020-09-10 15:15:38 +0900

    フォームに表示ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :destroy]
   before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :edit_basic_info]
-  before_action :correct_user, only: [:update]
+  before_action :correct_user, only: [:edit,:update]
   before_action :set_one_month, only: :show
   before_action :admin_user, only: [:destroy, :edit_basic_info]
 
