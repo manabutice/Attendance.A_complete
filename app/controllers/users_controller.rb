093d079e0387887a0fcf97536900241571c8eb7f
reo66 <8409.reo@gmail.com>
2020-09-15 21:55:01 +0900

    users_controller def import 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,11 @@ class UsersController < ApplicationController
     @users = User.all
   end  
 
-  
+  def import
+    # fileはtmpに自動で一時保存される
+    User.import(params[:file])
+    redirect_to users_url
+  end
 
 
 
