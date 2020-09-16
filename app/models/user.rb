bb225074ef479c0b1c160e585548150f047d273f
reo66 <8409.reo@gmail.com>
2020-09-16 23:24:43 +0900

    index.html.erb method: :post ついか

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,11 @@ class User < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      user = find_by(id: row["id"]) || new
+      if user = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
       user.attributes = row.to_hash.slice(*updatable_attributes)
       user.save
+      end
     end
   end
 
