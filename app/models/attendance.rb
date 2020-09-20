ceef785514d35f76b1530dc7fb927a552fab41c0
reo66 <8409.reo@gmail.com>
2020-09-20 14:09:48 +0900

    表の型はできた

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -10,11 +10,12 @@ class Attendance < ApplicationRecord
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
 
+
     def self.to_csv
-      headers = %w(ID 日付 出社 退社)
+      headers = %w(日付 出社 退社)
       csv_data = CSV.generate(headers: headers, write_headers: true, force_quotes: true) do |csv|
         all.each do |row|
-          csv << row.attributes.values_at(*self.column_names)
+          csv << ["column1", "column2", "column3"]
         end
       end
       csv_data.encode(Encoding: :SJIS)   
