f7be2f0bf54e02d93321ad8e8e7c2f63a9fc5649
reo66 <8409.reo@gmail.com>
2020-09-22 23:13:02 +0900

    attendance.rb validate追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,8 +5,15 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
 
   validate :finished_at_is_invalid_without_a_started_at
+  validate :started_at_than_finished_at_fast_if_invalid
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+
+  def started_at_than_finished_at_fast_if_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
   end
 end
