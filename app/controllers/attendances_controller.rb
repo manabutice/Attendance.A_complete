1498900e24f47710fd60de3d1e51b6462a00c247
reo66 <8409.reo@gmail.com>
2020-09-22 22:56:44 +0900

    attendance_conroller def update_one_month追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -28,4 +28,7 @@ class AttendancesController < ApplicationController
 
   def edit_one_month
   end 
+
+  def update_one_month
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
         patch 'update_basic_info'
         patch 'update_index'
         get 'attendances/edit_one_month'
-      patch 'attendances/update_one_month'
+        patch 'attendances/update_one_month'
       end
       collection do
         get 'working'
