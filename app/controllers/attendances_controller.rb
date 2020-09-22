bf4422b80b3692fe307162622a24b4511de8ab7a
reo66 <8409.reo@gmail.com>
2020-09-22 23:17:54 +0900

    attendance_controller update_one_month 記入2

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -30,10 +30,18 @@ class AttendancesController < ApplicationController
   end 
 
   def update_one_month
-    attendances_params.each do |id, item|
-      attendance = Attendance.find(id)
-      attendance.update_attributes!(item)
-  end 
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
 
 
 
