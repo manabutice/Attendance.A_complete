952438dac373a1b696e9faa644dcf629b87a626a
reo66 <8409.reo@gmail.com>
2020-10-02 09:06:48 +0900

    残業申請バリデーション記入

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,6 +4,11 @@ class Attendance < ApplicationRecord
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
 
+  # 残業申請バリデーション
+  validates :overtime_finished_at, presence: true
+  validates :indicater_check, presence: true
+  
+
   validate :finished_at_is_invalid_without_a_started_at
   validate :started_at_than_finished_at_fast_if_invalid
 
