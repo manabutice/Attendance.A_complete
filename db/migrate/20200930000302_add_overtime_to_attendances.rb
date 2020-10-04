da90fd0b903a062c374c6384d7db099dae9df261
reo66 <8409.reo@gmail.com>
2020-10-04 13:13:30 +0900

    支持者確認一応出た

--- db/migrate/20200930000302_add_overtime_to_attendances.rb
+++ db/migrate/20200930000302_add_overtime_to_attendances.rb
@@ -4,7 +4,9 @@ class AddOvertimeToAttendances < ActiveRecord::Migration[5.2]
     add_column :attendances, :tomorrow, :boolean
     add_column :attendances, :overtime_worked_on, :datetime
     add_column :attendances, :overtime_work, :string
+    # 指示者に申請ををしているかを確認するカラム
     add_column :attendances, :indicater_check, :boolean
+    # どの上長に残業申請をしているか確認するカラム
     add_column :attendances, :indicater_check_superior, :boolean
   end
 end
