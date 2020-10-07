74aa2057b8d0e5989fa125f287b42b29203043a7
reo66 <8409.reo@gmail.com>
2020-10-07 11:52:07 +0900

    残業申請のお知らせモーダルのカラム追加

---
+++ db/migrate/20201007024937_add_overtime_notice_to_attendances.rb
@@ -0,0 +1,11 @@
+# 残業申請のお知らせモーダル
+class AddOvertimeNoticeToAttendances < ActiveRecord::Migration[5.2]
+  def change
+    # 指示者確認
+    add_column :attendances, :indicater_reply, :string
+    # 変更
+    add_column :attendances, :change, :boolean
+    # 確認
+    add_column :attendances, :verification, :boolean
+  end
+end
