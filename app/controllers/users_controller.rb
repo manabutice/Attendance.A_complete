ea3f9bd89d9237f23b73d0e1187962f2df700d39
reo66 <8409.reo@gmail.com>
2020-12-14 11:31:32 +0900

    ユーザー新規作成修正ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,6 +76,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
